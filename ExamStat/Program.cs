26fe6216a065f394f4a129539ed7a2e4ee47e928
Rh Proj <r.proj@hotmail.com>
2021-06-11 12:11:23 +0300

    Add project files.

---
+++ ExamStat/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamStat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"\\fsttr02\стат. отчеты\Бланки отчетов\Старая папка\ts\";
+            string fileName = "var3.exe";
+
+            if (File.Exists(path + fileName))
+            {
+                Process.Start(path + fileName);
+            }
+            else
+            {
+                Console.WriteLine("Что-то пошло не так," + Environment.NewLine + "Либо Ваши действия несанкционированны," + Environment.NewLine + "Нажмите любую клавишу...");
+
+                try
+                {
+                    string userName = Environment.UserName;
+                    string machineName = Environment.MachineName;
+                    string localIP;
+
+                    using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+                    {
+                        socket.Connect("8.8.8.8", 65530);
+                        IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                        localIP = endPoint.Address.ToString();
+                    }
+
+                    using (StreamWriter sw = File.AppendText(path + "log.txt"))
+                    {
+                        sw.WriteLine($"{DateTime.Now} : {userName}, {localIP}, {machineName}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ ExamStatCore/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ExamStatCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Process.Start(@"\\fsttr02\стат. отчеты\Бланки отчетов\Старая папка\tst\testStat_var3.exe");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Что-то пошло не так..." + Environment.NewLine + Environment.NewLine + ex + Environment.NewLine + Environment.NewLine + "Нажмите чё-нить чтоб закрыть");
+                Console.ReadKey();
+            }
+        }
+    }
+}
